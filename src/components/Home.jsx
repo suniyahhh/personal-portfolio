d719fd0bcda5e74f777956a128277669f4024ff0
suniyahhh <123817061+suniyahhh@users.noreply.github.com>
2025-12-14 15:46:11 +0400

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     >
       <div className="max-w-5xl mx-auto">
         <h2 className="text-5xl font-bold mb-4">
-          Hi, I'm <span className="text-accent dark:text-darkAccent">Suniyah</span>.
+          Hi, I'm <span className="text-accent dark:text-darkAccent">Suniyah</span>
         </h2>
         <p className="text-xl text-foreground/80 dark:text-gray-300 max-w-xl leading-relaxed">
           A software engineer focused on cybersecurity, data, AI, and clean UI.  
